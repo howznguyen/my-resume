9702d9ff6baa069ebc956a53f0cebfef4807a272
Howz Nguyen <duyntp2000@gmail.com>
2023-01-27 23:16:27 +0700

    feat: create my resume html

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
